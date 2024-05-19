91ae7ab64475ac0d353c87f3256004fa29826a9d
kid-us <kidushh29@gmail.com>
2024-05-19 11:23:32 +0300

    Fetching Platform Icons

--- src/components/Games/PlatformIcons.tsx
+++ src/components/Games/PlatformIcons.tsx
@@ -1,3 +1,47 @@
+// import { Platform } from "../../hooks/useGames";
+// import {
+//   FaWindows,
+//   FaPlaystation,
+//   FaLinux,
+//   FaAndroid,
+//   FaXbox,
+//   FaApple,
+// } from "react-icons/fa";
+
+// import { MdPhoneIphone } from "react-icons/md";
+// import { BsGlobe } from "react-icons/bs";
+// import { SiNintendo } from "react-icons/si";
+// import { IconType } from "react-icons";
+
+// interface Props {
+//   platform: Platform[];
+// }
+
+// const PlatformIcons = ({ platform }: Props) => {
+//   const icons: { [key: string]: IconType } = {
+//     pc: FaWindows,
+//     playstation: FaPlaystation,
+//     xbox: FaXbox,
+//     nintendo: SiNintendo,
+//     mac: FaApple,
+//     linux: FaLinux,
+//     ios: MdPhoneIphone,
+//     web: BsGlobe,
+//     android: FaAndroid,
+//   };
+
+//   return (
+//     <>
+//       {platform.map((platform) => (
+//         <span>{icons[platform.slug]}</span>
+//       ))}
+//     </>
+//   );
+// };
+
+// export default PlatformIcons;
+
+import React from "react";
 import { Platform } from "../../hooks/useGames";
 import {
   FaWindows,
@@ -7,7 +51,6 @@ import {
   FaXbox,
   FaApple,
 } from "react-icons/fa";
-
 import { MdPhoneIphone } from "react-icons/md";
 import { BsGlobe } from "react-icons/bs";
 import { SiNintendo } from "react-icons/si";
@@ -17,7 +60,7 @@ interface Props {
   platform: Platform[];
 }
 
-const PlatformIcons = ({ platform }: Props) => {
+const PlatformIcons: React.FC<Props> = ({ platform }) => {
   const icons: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
@@ -32,9 +75,14 @@ const PlatformIcons = ({ platform }: Props) => {
 
   return (
     <>
-      {platform.map((platform) => (
-        <span>{icons[platform.slug]}</span>
-      ))}
+      {platform.map((p) => {
+        const IconComponent = icons[p.slug];
+        return IconComponent ? (
+          <span key={p.slug}>
+            <IconComponent />
+          </span>
+        ) : null;
+      })}
     </>
   );
 };
