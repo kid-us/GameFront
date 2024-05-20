8e0b945a7fc463527bfbb6f5eb356f625134f0c3
kid-us <kidushh29@gmail.com>
2024-05-20 09:21:57 +0300

    Game cards

--- src/components/Genres/Genres.tsx
+++ src/components/Genres/Genres.tsx
@@ -1,17 +1,22 @@
 import useGenres from "../../hooks/useGenres";
+import { Link } from "react-router-dom";
 
 const Genres = () => {
   const genres = useGenres();
   return (
     <>
       {genres?.map((genre) => (
-        <div key={genre.id} className="grid grid-cols-3 mb-5">
+        <Link
+          to={`/${genre.id}`}
+          key={genre.id}
+          className="grid grid-cols-3 mb-5"
+        >
           <img
             src={genre.image_background}
             className="w-10 object-cover h-10 rounded shadow-lg"
           />
           <p className="mt-2">{genre.name}</p>
-        </div>
+        </Link>
       ))}
     </>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,11 +9,11 @@ const Hero = () => {
         className="grid lg:grid-cols-6 md:grid-cols-6"
         style={{
           backgroundSize: "cover",
-          backgroundImage: `linear-gradient(to bottom, rgba(1, 1, 1, 0.70), rgba(0, 0, 0, 0.99))`,
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.99),rgba(0, 0, 0, 0.99) )`,
         }}
       >
         {/* Genres */}
-        <div className="container-fluid shadow-xl bg-black text-white font-semibold lg:block md:block hidden border-gray-400-700 me-5 py-10">
+        <div className="container-fluid shadow-xl bg-white text-black font-semibold lg:block md:block hidden border-gray-400-700 me-5 py-10">
           <h1 className="font-bold  bi-dpad-fill text-2xl"> Games</h1>
           <p className="text-sm font-semibold mt-2">420 Results</p>
           <hr className="my-5" />
