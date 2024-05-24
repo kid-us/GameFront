8b22b0bc6de0898ee265e70fe7ebf46952c7c430
kid-us <kidushh29@gmail.com>
2024-05-24 17:16:20 +0300

    Similar Games

--- src/components/GameDetail/SimilarGames.tsx
+++ src/components/GameDetail/SimilarGames.tsx
@@ -9,20 +9,26 @@ interface Props {
 const SimilarGames = ({ name, similarGame }: Props) => {
   return (
     <div>
-      <p className="text-lg mb-5 font-poppins text-gray-400">
+      <p className="text-lg my-10 font-poppins text-gray-400">
         Games like <span className="text-teal-400 font-poppins">{name}</span>
       </p>
 
-      <div className="grid grid-cols-7 gap-4">
+      <div className="grid grid-cols-5 gap-4">
         {similarGame.map((similar) => (
-          <Link to={`/${similar.id}`} key={similar.id} className="">
+          <Link
+            to={`/${similar.id}`}
+            key={similar.id}
+            className="hover:-translate-y-2 duration-500"
+          >
             <img
               src={similar.background_image}
               alt="Similar Games"
-              className="aspect-square object-cover rounded"
+              className="aspect-square object-cover rounded grayscale-[50%] hover:grayscale-0"
             />
             <div className="flex">
-              <p className="font-poppins text-xs mt-2">{similar.name}</p>
+              <p className="font-poppins text-xs mt-2 text-gray-400">
+                {similar.name}
+              </p>
             </div>
           </Link>
         ))}
