b82854542e3dfc3455c5640a47beba1efef16e67
kid-us <kidushh29@gmail.com>
2024-05-24 22:03:35 +0300

    Adding Loading

---
+++ src/components/GameDetail/Loading.tsx
@@ -0,0 +1,46 @@
+import { photo } from "../../assets/img";
+
+const Loading = () => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `linear-gradient(to bottom, rgba(1, 1, 1, 0.80), rgba(0, 0, 0, 0.99))`,
+          backgroundPosition: "center top",
+          paddingTop: "40px",
+        }}
+      >
+        <div className="lg:container lg:mx-auto">
+          <h1 className="lg:text-xl text-xl mb-6 lg:mx-0 mx-2 bg-zinc-600 w-2/5 h-8 rounded"></h1>
+
+          {/* Info */}
+          <div className="lg:flex lg:gap-5 justify-between">
+            <div className="lg:w-[80%] lg:mx-0 mx-2">
+              <img
+                src={photo}
+                alt="Game"
+                className="lg:aspect-video aspect-square sm:aspect-square shadow shadow-teal-200 h-[90%] w-full object-cover rounded-lg"
+              />
+            </div>
+            <div className="lg:w-[20%] bg-zinc-950 rounded-md px-5 py-5 text-sm mb-14">
+              <p className="mb-5 bg-zinc-600 w-full rounded h-6"></p>
+              <p className="bg-zinc-600  w-full rounded h-6"></p>
+              <p className="my-4 bg-zinc-600  w-full rounded h-6"></p>
+              <p className="mb-1 mt-5 bg-zinc-600  w-full rounded h-6"></p>
+              <p className="font-mono bg-zinc-600  w-full rounded h-6"></p>
+              <p className="mb-1 mt-5 bg-zinc-600  w-full rounded h-6"></p>
+              <p className="bg-zinc-600"></p>
+              <img src={photo} alt="Preview" className="rounded mt-8" />
+              <div className="flex space-x-4 mt-10">
+                <p className="bg-zinc-600  w-full rounded h-6"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/Games/GameDetail.tsx
+++ src/components/Games/GameDetail.tsx
@@ -10,6 +10,7 @@ import Tags from "../GameDetail/Tags";
 import Stores from "../GameDetail/Stores";
 import Developer from "../GameDetail/Developer";
 import SimilarGames from "../GameDetail/SimilarGames";
+import Loading from "../GameDetail/Loading";
 
 // Game Info
 interface Store {
@@ -109,57 +110,73 @@ const GameDetail = () => {
   const [gameDetail, setGameDetail] = useState<GameDetails>();
   const [screenshot, setScreenshot] = useState<Screenshots[]>([]);
   const [similarGames, setSimilarGames] = useState<Similar[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     apiClient.get<GameDetails>(`/games/${id}`).then((res) => {
       setGameDetail(res.data);
+      setLoading(false);
     });
     apiClient
       .get<ScreenshotResponse>(`/games/${id}/screenshots`)
       .then((res) => {
         setScreenshot(res.data.results);
+        setLoading(false);
       });
     apiClient.get<SimilarGamesFetch>(`/games/${id}/game-series`).then((res) => {
       setSimilarGames(res.data.results);
+      setLoading(false);
     });
   }, []);
 
   return (
     <>
-      <Navbar />
-      {gameDetail && (
-        <div>
-          <GameIntro gameDetail={gameDetail} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Navbar />
+          {gameDetail && (
+            <div>
+              <GameIntro gameDetail={gameDetail} />
 
-          <div className="container mx-auto text-white mt-5">
-            <div className="lg:grid lg:grid-cols-3 justify-between lg:gap-10">
-              {/* Description  and Stores*/}
-              <div className="lg:col-span-2 lg:px-0 px-3">
-                <Description description={gameDetail.description} />
-              </div>
-              <div className="mt-10 bg-zinc-900 rounded p-4">
-                <Stores store={gameDetail} />
+              <div className="container mx-auto text-white mt-5">
+                <div className="lg:grid lg:grid-cols-3 justify-between lg:gap-10">
+                  {/* Description  and Stores*/}
+                  <div className="lg:col-span-2 lg:px-0 px-3">
+                    <Description description={gameDetail.description} />
+                  </div>
+                  <div className="mt-10 bg-zinc-900 rounded p-4">
+                    <Stores store={gameDetail} />
+                  </div>
+                </div>
+                {/* Screenshot */}
+                {screenshot && <Screenshot screenshots={screenshot} />}
+                {/* Developers */}
+                <div className="lg:grid lg:grid-cols-2 lg:px-0 px-3">
+                  <Developer devs={gameDetail.developers} />
+                  <Tags tag={gameDetail.tags} />
+                </div>
+                {/* System Requirements */}
+                {Object.keys(gameDetail.platforms[0].requirements).length !=
+                  0 && (
+                  <SystemRequirements
+                    minimum={gameDetail.platforms[0].requirements.minimum}
+                    recommended={
+                      gameDetail.platforms[0].requirements.recommended
+                    }
+                  />
+                )}
+
+                {/* Similar Games */}
+                <SimilarGames
+                  name={gameDetail.name}
+                  similarGame={similarGames}
+                />
               </div>
             </div>
-            {/* Screenshot */}
-            {screenshot && <Screenshot screenshots={screenshot} />}
-            {/* Developers */}
-            <div className="lg:grid lg:grid-cols-2 lg:px-0 px-3">
-              <Developer devs={gameDetail.developers} />
-              <Tags tag={gameDetail.tags} />
-            </div>
-            {/* System Requirements */}
-            {Object.keys(gameDetail.platforms[0].requirements).length != 0 && (
-              <SystemRequirements
-                minimum={gameDetail.platforms[0].requirements.minimum}
-                recommended={gameDetail.platforms[0].requirements.recommended}
-              />
-            )}
-
-            {/* Similar Games */}
-            <SimilarGames name={gameDetail.name} similarGame={similarGames} />
-          </div>
-        </div>
+          )}
+        </>
       )}
     </>
   );
