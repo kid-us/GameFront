e86da11f588eee57b509332bbc5a301d80ec3a0e
kid-us <kidushh29@gmail.com>
2024-05-29 12:48:55 +0300

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <footer className="h-auto footer-bg pt-14 me-7">
+      <footer className="h-auto footer-bg pt-24 pb-14 me-7">
         <div className="container mx-auto ">
           <div className="grid grid-cols-3">
             <div>
@@ -9,6 +9,10 @@ const Footer = () => {
                 <span className="bi-controller me-4"></span>
                 GameFront
               </p>
+              <p className="mt-10 text-gray-500 text-sm font-semibold">
+                &copy; 2024 All right reserved
+              </p>
+            
             </div>
             <div className="col-span-2">
               <div className="flex justify-between">
@@ -23,12 +27,16 @@ const Footer = () => {
                 </div>
               </div>
               <div className="mt-10">
-                <p>
-                  GameFront is top of free Movie and Tv Shows static website,
-                  where to watch movies trailers and all information online free
-                  without registration required. With a big database and great
-                  features, we're confident Vidly is the best free movies online
+                <p className="text-gray-500 font-semibold text-sm">
+                  GameFront is top of free video games static website, where to
+                  watch games trailers and all information online free without
+                  registration required. With a big database and great features,
+                  we're confident GameFront is the best free video games online
                   website in the space that you can't simply miss!
+                </p>
+                <p className="mt-5 text-gray-500 font-semibold text-sm">
+                  This site does not store any files on our server, we only
+                  linked to the media which is hosted on 3rd party services.
                 </p>
               </div>
             </div>
