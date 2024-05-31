e690756347928d71545f1d679ca7aa8ef56260bb
kid-us <kidushh29@gmail.com>
2024-05-31 11:46:49 +0300

    Adding route for 404 Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import GameDetail from "./components/Games/GameDetail";
+import Page404 from "./components/Page404";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/:id" element={<GameDetail />}></Route>
+        <Route path="/404" element={<Page404 />}></Route>
+        <Route path="*" element={<Navigate to="/404" />}></Route>
       </Routes>
     </>
   );
--- src/components/Page404.tsx
+++ src/components/Page404.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Page404 = () => {
   return (
     <>
-      <div className="container text-center">
+      <div className="container mx-auto text-center">
         <div
           className=""
           style={{
@@ -19,14 +19,14 @@ const Page404 = () => {
             }}
             className="fw-semibold"
           >
-            <h1>404 | Page</h1>
-            <p className="mt-5 text-gray-400 text-sm">
+            <h1 className="font-poppins text-white text-3xl">404 | Page</h1>
+            <p className="mt-5 text-gray-400 text-sm font-poppins">
               The Server can not find the requested page
             </p>
-            <p className="text-center text-sm mt-5">
+            <p className="text-center text-sm mt-10">
               <Link
                 to={"/"}
-                className="bg-teal-700 py-2 px-5 rounded text-white mt-5 text-sm"
+                className="bg-teal-700 py-3 px-5 rounded text-white text-sm font-poppins"
               >
                 Return to Home
               </Link>
