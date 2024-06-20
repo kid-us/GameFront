90cad0d1a0ae8c49db50544655cd81425a50a553
kid-us <kidushh29@gmail.com>
2024-06-20 14:05:51 +0300

    Responsive breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1280px",
+
+      "3xl": "1280px",
+
+      "4xl": "1280px",
+
+      "5xl": "1280px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
