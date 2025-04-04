a30d49e7dffb1a85f3bf3908fc2dbe87de411dc6
kid-us <kidushh29@gmail.com>
2025-04-04 11:21:10 +0300

    Navbar fixed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -24,15 +24,13 @@ const Navbar = () => {
   }, []);
 
   return (
-    <>
-      <div
-        className={`${
-          show ? "bg-zinc-950" : "nav-bg"
-        } fixed z-10 w-full py-4 hover:bg-zinc-950`}
-      >
-        <div
-          className={`flex flex-shrink-0 justify-between md:mx-6 lg:mx-12 mx-3`}
-        >
+    <div
+      className={`${
+        show ? "bg-zinc-950" : "nav-bg"
+      } fixed z-10 w-full py-4 hover:bg-zinc-950 `}
+    >
+      <div className="container mx-auto">
+        <div className={`flex flex-shrink-0 justify-between lg:mx-0 mx-6`}>
           <div className="flex justify-start gap-28">
             <div>
               <Link to="/" className="flex text-white font-poppins text-lg">
@@ -67,7 +65,7 @@ const Navbar = () => {
           </div>
         </>
       )}
-    </>
+    </div>
   );
 };
 
